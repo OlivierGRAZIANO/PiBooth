2616bd7306c6aac8c1e9da2f4899c453a272d604
Olivier GRAZIANO <dev@graziano.pro>
2014-09-27 22:04:39 +0000

    mise en place twig, gpio, socket.io

--- server.js
+++ server.js
@@ -1,15 +1,61 @@
-// SET UP ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var express	 = require('express');
 var app 	 = express();
+var server = require('http').Server(app);
 var port 	 = process.env.PORT || 8080;
 
-// ROUTING ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var gpio = require('gpio');
+var io = require('socket.io')(server);
 
-require('./app/routes.js')(app);
+server.listen(port);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+app.get('/', function(req, res) {
+
+	res.render('index.html.twig');
+});
 
 
-// START SERVER //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+io.sockets.on('connection', function(socket){
 
-app.listen(port);
+  console.log("Connexion");
+
+});
+
+var button = gpio.export(4, {
+   direction: "in",
+   ready: function() {
+   	console.log('4 in');
+   }
+});
+
+var gpio17 = gpio.export(17, {
+   direction: "out",
+   ready: function() {
+   	console.log('17 out');
+   }
+});
+
+
+button.on("change", function(val) {
+   	  
+    var state = Math.abs(val-1) === 1;
+
+    //debug
+  	gpio17.set(Math.abs(val-1), function() {
+   		console.log(gpio17.value);    // should log 0
+	  });
+
+    if(state)
+    {
+      io.sockets.emit("photo");
+    }
+
+});
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//app.listen(port);
 console.log('Server started on port '+port);
